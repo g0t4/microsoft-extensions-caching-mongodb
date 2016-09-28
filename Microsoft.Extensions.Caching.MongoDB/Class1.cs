7f5f93c55f21fdba64789ff3f50eb6ec044cb858
Wes Higbee <wes.mcclure@gmail.com>
2016-09-28 11:46:32 -0400

    Add project files.

---
+++ Microsoft.Extensions.Caching.MongoDB/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.Caching.MongoDB
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
---
+++ Microsoft.Extensions.Caching.MongoDB/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Microsoft.Extensions.Caching.MongoDB")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bbbcd78b-e884-4b3a-a70d-47eb2df298d8")]
