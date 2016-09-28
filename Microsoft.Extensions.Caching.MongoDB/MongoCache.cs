a4699927e90818169739fb5124e487c899f6ad07
Wes Higbee <wes.mcclure@gmail.com>
2016-09-28 12:18:56 -0400

    Setup project.json and pull in dependencies to Caching.Abstractions - add skeleton MongoCache

---
+++ Microsoft.Extensions.Caching.MongoDB/MongoCache.cs
@@ -0,0 +1,48 @@
+﻿namespace Microsoft.Extensions.Caching.MongoDB
+{
+	using System;
+	using System.Threading.Tasks;
+	using Distributed;
+	public class MongoCache : IDistributedCache
+	{
+		public byte[] Get(string key)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<byte[]> GetAsync(string key)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Refresh(string key)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task RefreshAsync(string key)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Remove(string key)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task RemoveAsync(string key)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Set(string key, byte[] value, DistributedCacheEntryOptions options)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task SetAsync(string key, byte[] value, DistributedCacheEntryOptions options)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
