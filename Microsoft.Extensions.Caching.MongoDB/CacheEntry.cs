1f7548174b1ccac5a40139106aef95edad0dc2b0
Wes Higbee <wes.mcclure@gmail.com>
2016-09-29 09:43:23 -0400

    Notes about possible expiration types for a cache entry

--- Microsoft.Extensions.Caching.MongoDB/CacheEntry.cs
+++ Microsoft.Extensions.Caching.MongoDB/CacheEntry.cs
@@ -3,6 +3,14 @@
 	using System;
 	using global::MongoDB.Bson.Serialization.Attributes;
 
+	/// <summary>
+	///     Four possible expiration types:
+	///     - None (neither absolute nor sliding are set) - lives until removed
+	///     - Absolute Only (sliding is not set) - lives until specific time
+	///     - Sliding Only (absolute not set) - lives for duration of sliding window, extends life if accessed before current
+	///     window ends (last access + sliding window duration)
+	///     - Slide Until (sliding and absolute are both set) - like Sliding Only with an absolute end to the sliding
+	/// </summary>
 	public class CacheEntry
 	{
 		/// <summary>
@@ -13,7 +21,11 @@
 		public string Key { get; set; }
 
 		public byte[] Value { get; set; }
+
 		public DateTimeOffset? AbsolutionExpiration { get; set; }
+
 		public TimeSpan? SlidingExpiration { get; set; }
+
+		public DateTimeOffset LastAccessedAt { get; set; }
 	}
 }
